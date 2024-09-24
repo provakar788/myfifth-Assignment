0f13f3ce5f8fb7223b54d9b6931e009099da37ec
provakar1 <provakarsaha788@gmail.com>
2024-09-24 14:27:56 +0600

    added the quota movement donation calculation function

--- js/index.js
+++ js/index.js
@@ -86,3 +86,47 @@ document.getElementById('second-btn')
 
         donationHistoryItem.insertBefore(historyItem, donationHistoryItem.firstChild);
 });
+
+// Quota Movement donation card functionality
+document.getElementById('third-btn')
+    .addEventListener('click', function(){
+        // e.preventDefault();
+        const injuredDonateInput = parseFloat(getInputValueById('injured-donate-input'));
+        const donateInjuredValue = parseFloat(setTextValueById('donate-injured-value'));
+        const donationMinus = parseFloat(donationValueMinus('donation-minus'));
+        
+        // error message
+        const modal1 = document.getElementById('my_modal_1');
+        const modal2 = document.getElementById('my_modal_2');
+
+        if(isNaN(injuredDonateInput) || injuredDonateInput <= 0 || typeof injuredDonateInput === null){
+            modal2.showModal();
+            return;
+        }else if(typeof injuredDonateInput === 'number'){
+            modal1.showModal();
+        }
+
+        // injured donation value update
+        const injuredTotalValue = injuredDonateInput + donateInjuredValue;
+        document.getElementById('donate-injured-value').innerText = injuredTotalValue;
+
+        // donation value minus
+        const remainingValue = donationMinus - injuredDonateInput;
+        document.getElementById('donation-minus').innerText = remainingValue;  
+
+        // input clear
+        document.getElementById('injured-donate-input').value = '';
+        
+        // history page
+        const donationHistoryItem = document.getElementById('history-div');
+        const historyItem = document.createElement('div');
+        historyItem.classList.add('lg:w-7/12','w-full','border','rounded-xl','p-4','bg-white','text-center','space-y-4','mx-auto') ;                            
+        historyItem.innerHTML = `
+                            <div class="flex gap-5 justify-center">
+                                <p class="text-sm opacity-75">Date : ${new Date().toLocaleDateString()}</p>
+                                <p class="text-sm opacity-75">Time : ${new Date().toLocaleTimeString()}</p>
+                            </div>
+                            <p class="font-bold">${injuredDonateInput} Taka is Donated for Aid for injured in the Quota Movement, Bangladesh.</p>`;
+
+        donationHistoryItem.insertBefore(historyItem, donationHistoryItem.firstChild);
+});
