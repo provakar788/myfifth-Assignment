9fc433de844a122477216b1e67b1e2588dc0492a
provakar1 <provakarsaha788@gmail.com>
2024-09-24 14:25:39 +0600

    added the feni donation calculation function

--- js/index.js
+++ js/index.js
@@ -41,3 +41,48 @@ document.getElementById('first-btn')
 
         donationHistoryItem.insertBefore(historyItem, donationHistoryItem.firstChild);
 });
+
+// Feni donation card functionality
+document.getElementById('second-btn')
+    .addEventListener('click', function(){
+        // e.preventDefault();
+        const feniDonateInput = parseFloat(getInputValueById('feni-donate-input'));
+        const donateFeniValue = parseFloat(setTextValueById('donate-feni-value'));
+        const donationMinus = parseFloat(donationValueMinus('donation-minus'));
+
+        // error message
+        const modal1 = document.getElementById('my_modal_1');
+        const modal2 = document.getElementById('my_modal_2');
+
+        if(isNaN(feniDonateInput) || feniDonateInput <= 0 || typeof feniDonateInput === null){
+            modal2.showModal();
+            return;
+        }else if(typeof feniDonateInput === 'number'){
+            modal1.showModal();
+        }
+
+        // feni donation value update
+        const feniTotalValue = feniDonateInput + donateFeniValue;
+        document.getElementById('donate-feni-value').innerText = feniTotalValue;
+
+        // feni value minus
+        const remainingValue = donationMinus - feniDonateInput;
+        document.getElementById('donation-minus').innerText = remainingValue; 
+        
+        // input clear
+        document.getElementById('feni-donate-input').value = '';
+        
+        // history page
+        const donationHistoryItem = document.getElementById('history-div');
+        const historyItem = document.createElement('div');
+        historyItem.classList.add('lg:w-7/12','w-full','border','rounded-xl','p-4','bg-white','text-center','space-y-4','mx-auto') ;
+        historyItem.innerHTML = `
+                            <div class="flex gap-5 justify-center">
+                                <p class="text-sm opacity-75">Date : ${new Date().toLocaleDateString()}</p>
+                                <p class="text-sm opacity-75">Time : ${new Date().toLocaleTimeString()}</p>
+                            </div>
+                            <p class="font-bold">${feniDonateInput} Taka is Donated for Flood Relief in Feni, Bangladesh.</p>`;
+
+
+        donationHistoryItem.insertBefore(historyItem, donationHistoryItem.firstChild);
+});
--- js/utilities.js
+++ js/utilities.js
@@ -2,3 +2,11 @@
 function getInputValueById(id){
     return document.getElementById(id).value;
 }
+
+function setTextValueById(id){
+    return document.getElementById(id).innerText;
+}
+
+function donationValueMinus(id){
+    return document.getElementById(id).innerText;
+}
